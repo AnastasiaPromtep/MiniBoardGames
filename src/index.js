066e7ad71447ccc9d3ea3954922b32271d3852d8
Anastasia Prom tep <apromtep@gmail.com>
2020-02-03 15:13:59 +0100

    Add Draw state

--- src/index.js
+++ src/index.js
@@ -116,12 +116,15 @@ class Game extends React.Component {
       );
     });
 
-    let status;
+    let status = '';
+
     if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+        status = 'Winner: ' + winner;
+      } else if (this.state.stepNumber === 9) {
+          status = 'Draw !'
+      } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
     return (
       <div className="game">
